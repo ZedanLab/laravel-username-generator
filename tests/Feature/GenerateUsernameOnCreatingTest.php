247642e591dc1ec5fc24883ae94683264c540428
Mohamed Zedan <mo@zedan.me>
2022-06-06 17:32:51 +0200

    update run_tests action

--- tests/Feature/GenerateUsernameOnCreatingTest.php
+++ tests/Feature/GenerateUsernameOnCreatingTest.php
@@ -19,27 +19,3 @@ it('generates a unique username on creating', function () {
     expect($user->username != $anotherUser->username)->toBeTrue();
     expect(Str::startsWith($anotherUser->username, $user->username))->toBeTrue();
 });
-
-// it('generates a username on updating if it is enabled', function () {
-//     User::setUsernameGeneratorOptions(['on_updating' => true, 'source' => 'name']);
-
-//     $user = User::factory()->create(['name' => 'John']);
-//     $oldUsername = $user->username;
-
-//     $user->update(['name' => 'Doe']);
-//     $newUsername = $user->username;
-
-//     expect($oldUsername !== $newUsername)->toBeTrue();
-// });
-
-// it('not generates a username on updating if it is disabled', function () {
-//     User::setUsernameGeneratorOptions(['on_updating' => false]);
-
-//     $user = User::factory()->create(['name' => 'John']);
-//     $oldUsername = $user->username;
-
-//     $user->update(['name' => 'Doe']);
-//     $newUsername = $user->username;
-
-//     expect($oldUsername == $newUsername)->toBeTrue();
-// });
